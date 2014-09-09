1586019cf82455d7ea18049eb3018f23ee21e177
Phinocio <devon.r.mcclure@gmail.com>
2014-09-09 15:48:54 -0700

    Fixed minor issue and deleted unused controller

--- app/controllers/PackGeneratorController.php
+++ app/controllers/PackGeneratorController.php
@@ -1,5 +1,7 @@
 <?php
 
+use MagicPackGenerator\Card\Card;
+
 class PackGeneratorController extends BaseController {
 
     /**
@@ -7,10 +9,14 @@ class PackGeneratorController extends BaseController {
      * @param  array $sortedSet The sorted set from PackSortController
      * @return [type]            [description]
      */
-    public function getPack($sortedSet)
+    public function getPack($set = 'm13', $cardsInSet = 249)
     {
         // TODO: Make sure no duplicates in pack
         // TODO: Add foils and allow duplicate if it's a foil
+        $cards = new Card;
+        $cards->search($set, $cardsInSet);
+        $cards->sort($cards->setCards);
+        $sortedSet = $cards->sortedSet;
 
         // 1 in 8 chance of a pack having a mythic.
         $rand = Rand(1,8);
@@ -45,79 +51,9 @@ class PackGeneratorController extends BaseController {
         {
             echo '<img src="http://manastack.com/cards/images/' . $card['set'] . '/' . $card['num'] . '.jpg" />';
         }
+
+        return View::make('index')
+                ->with('pack', $pack);
     }
 
 }
-
-
-    /*
-     * Created: 2014/05/07 @ 15:55 PST.
-     *
-     * Purpose: To create a random pack of magic cards from 28 different cards.
-     *
-     * TODO: Deal with duplicate cards in the pack.
-     */
-    /*
-    $pack = array();
-
-    $cardNames = [
-      'Force Of Will',
-      'Mana Drain',
-      'Uril The Mistalker',
-      'Heritage Druid',
-      'Leyline Of Lifeforce',
-      'Counterspell',
-      'Terror',
-      'Blood Moon',
-      'Nicol Bolas',
-      'Pilli-Palla',
-      'Sol Ring',
-      'Copper Myr',
-      'Sword Of Kaladra',
-      'Darksteel Plate',
-      'Necropotence',
-      'Kessig Wolf-Run',
-      'Island',
-      'Bayou',
-      'Delver Of Secrets',
-      'Black Vice',
-      'Rain Of Gore',
-      'Ranger Of Eos',
-      'Horde Of Notions',
-      'Fireblast',
-      'Pyroblast',
-      'Blue Elemental Blast',
-      'Llanowar Elves',
-      'Birds Of Paradise'
-    ];
-
-    for($i = 0; $i < 15; $i++)
-    {
-        $randNum = rand(0,count($cardNames)-1);
-        $pack[$i] = $cardNames[$randNum];
-        checkDuplicate($pack, $cardNames, $i, $randNum);
-
-    }
-
-    for($ii = 0; $ii < count($pack); $ii++)
-    {
-        echo '++++' . $pack[$ii] . '<br />';
-    }
-
-    function checkDuplicate(&$pack, &$cardNames, $i, $randNum)
-    {
-        if(in_array($cardNames[$randNum], $pack))
-        {
-            //echo $cardNames[$randNum] . '-----<br />';
-            $randNum = rand(0,count($cardNames)-1);
-            $i - 1;
-            checkDuplicate($pack, $cardNames, $i, $randNum);
-        }
-        else
-        {
-            $pack[$i] = $cardNames[$randNum];
-            return $pack;
-        }
-    }
-?>
-*/
